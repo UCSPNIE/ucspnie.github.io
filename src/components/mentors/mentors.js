88363cf73533fa119efbe02d5b1f82b69745719b
AdityaSubrahmanyaBhat <70971391+AdityaSubrahmanyaBhat@users.noreply.github.com>
2021-12-03 19:05:36 +0530

    home page Fixes #4

---
+++ src/components/mentors/mentors.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./mentors.css"
+import Lottie from "react-lottie";
+import mentors from "../../lottie/mentor.json"
+
+const Mentors = () => {
+    const mentorsLottie = {
+        loop: true,
+        autoplay: true,
+        animationData: mentors,
+    };
+    const text='The people we go to , for they are the ones who have our backs.';
+    return (
+        <div className="mentors_row">
+            <div id="mentors" className="mentors_title">
+                <h1>MENTORS</h1>
+            </div>
+            <div className="mentors_content">
+                <span className="mentors_tb">
+                    <div className="mentors_text">{text}</div>
+                    <div className="mentors_button">
+                        <a href="#mentors" className="a">
+                            <nav className="nav">
+                            <ul className="nav_ul">
+                                <li className="nav_ul_li">
+                                    Meet Mentors
+                                    <span className="nav_ul_li_span"></span>
+                                </li>
+                            </ul>
+                        </nav></a>
+                    </div>
+                </span>
+                <span className="mentors_lottie">
+                    <Lottie
+                        options={mentorsLottie}
+                        height='50vh'
+                        width='40vw'
+                    />
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default Mentors;
