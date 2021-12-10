7d051e5808bd31b0af64e940a27af74113b97aba
Ankit Kumar <mail.ankitkumar14@gmail.com>
2021-12-11 00:56:11 +0530

    added footer and resolved home page issue and modified event page

---
+++ src/pages/footer/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="ucsp__footer section__padding">
+    <div className="ucsp__footer-heading">
+      <h1 className="gradient__text">
+        Do you want to step in to the future before others
+      </h1>
+    </div>
+
+    <div className="ucsp__footer-btn">
+      <p>JOIN US</p>
+    </div>
+
+    <div className="ucsp__footer-links">
+      <div className="ucsp__footer-links_logo">
+        <div className="image">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C510BAQFurjQnxyo2Og/company-logo_200_200/0/1565109886767?e=1647475200&v=beta&t=3l5foUIY5asrcnV1lKMbWaJp_9aziQy4NN-mASf7YB8"
+            alt="ucsp_logo"
+          />
+        </div>
+
+        <div className="info">
+          <p className="space">Group Info</p>
+          <p>Dept. of ECE, NRN Block</p>
+          <p>THE NATIONAL INSTITUTE OF ENGINEERING</p>
+          <p>Mananthvady Road</p>
+          <p>Mysuru - 570008</p>
+          <p>Karnataka</p>
+        </div>
+      </div>
+
+      <div className="ucsp__footer-links_div">
+        <h4 className="space">Get in touch</h4>
+        <p>Spoorthy J</p>
+        <p>+91 7026763209</p>
+        <p>ucsp.nie@gmail.com</p>
+        <div className="handles">
+          <span className="facebook">
+            <a href="">
+              <i class="fab fa-facebook"></i>
+            </a>
+          </span>
+          <span className="instagram">
+            <a href="">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </span>
+          <span className="twitter">
+            <a href="">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </span>
+          <span className="linkedin">
+            <a href="">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <div className="ucsp__footer-copyright">
+      <p>&#169; UCSP</p>
+    </div>
+  </div>
+);
+
+export default Footer;
