4cf117fff09ce5a28291bd82ed774cd4aebbad47
Chanakya <ucs3721@outlook.com>
2022-01-12 10:37:46 +0530

    Seperated `Button` component for reuse in `/home` page

--- src/components/events/events.js
+++ src/components/events/events.js
@@ -3,38 +3,30 @@ import "./events.css";
 import Lottie from "react-lottie-player";
 import events from "../../lottie/events.json";
 import { Link } from "react-router-dom";
+import Button from "../common/button";
 
 const Events = () => {
-  const text = "From a wide range of events conducted by our club , here are a few";
-  return (
-    <div className="events_row">
-      <div id="events" className="events_title">
-        <h1>EVENTS</h1>
-      </div>
-      <div className="events_content">
-        <span className="events_lottie">
-          <Lottie animationData={events} loop play />
-        </span>
-        <span className="events_tb">
-          <div className="events_text">{text}</div>
-          <div className="events_button">
-            <Link to="/events">
-              <a href="/events" className="a">
-                <nav className="nav">
-                  <ul className="nav_ul">
-                    <li className="nav_ul_li">
-                      Go to Events
-                      <span className="nav_ul_li_span"></span>
-                    </li>
-                  </ul>
-                </nav>
-              </a>
-            </Link>
-          </div>
-        </span>
-      </div>
-    </div>
-  );
+	const text = "From a wide range of events conducted by our club, here are a few";
+	return (
+		<div className="events_row">
+			<div id="events" className="events_title">
+				<h1>EVENTS</h1>
+			</div>
+			<div className="events_content">
+				<span className="events_lottie">
+					<Lottie animationData={events} loop play />
+				</span>
+				<span className="events_tb">
+					<div className="events_text">{text}</div>
+					<div className="events_button">
+						<Link to="/events">
+							<Button href="/events" text="GO TO EVENTS" />
+						</Link>
+					</div>
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default Events;
--- src/components/mentors/mentors.js
+++ src/components/mentors/mentors.js
@@ -3,38 +3,30 @@ import "./mentors.css";
 import Lottie from "react-lottie-player";
 import mentors from "../../lottie/mentor.json";
 import { Link } from "react-router-dom";
+import Button from "../common/button";
 
 const Mentors = () => {
-  const text = "The people we go to , for they are the ones who have our backs.";
-  return (
-    <div className="mentors_row">
-      <div id="mentors" className="mentors_title">
-        <h1>MENTORS</h1>
-      </div>
-      <div className="mentors_content">
-        <span className="mentors_tb">
-          <div className="mentors_text">{text}</div>
-          <div className="mentors_button">
-            <Link to="/mentors">
-              <a href="/mentors" className="a">
-                <nav className="nav">
-                  <ul className="nav_ul">
-                    <li className="nav_ul_li">
-                      Meet Mentors
-                      <span className="nav_ul_li_span"></span>
-                    </li>
-                  </ul>
-                </nav>
-              </a>
-            </Link>
-          </div>
-        </span>
-        <span className="mentors_lottie">
-          <Lottie animationData={mentors} loop play />
-        </span>
-      </div>
-    </div>
-  );
+	const text = "The people we go to , for they are the ones who have our backs.";
+	return (
+		<div className="mentors_row">
+			<div id="mentors" className="mentors_title">
+				<h1>MENTORS</h1>
+			</div>
+			<div className="mentors_content">
+				<span className="mentors_tb">
+					<div className="mentors_text">{text}</div>
+					<div className="mentors_button">
+						<Link to="/mentors">
+							<Button text="Meet Mentors" href="/mentors" />
+						</Link>
+					</div>
+				</span>
+				<span className="mentors_lottie">
+					<Lottie animationData={mentors} loop play />
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default Mentors;
